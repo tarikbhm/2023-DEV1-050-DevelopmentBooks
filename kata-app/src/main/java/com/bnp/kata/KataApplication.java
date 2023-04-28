003d6e8bde09cf9744eabe6b5755eb122b787675
Barhoumi <ts-tarik.barhoumi@rakuten.com>
2023-04-28 17:11:27 +0200

    First Commit init DDD modules

---
+++ kata-app/src/main/java/com/bnp/kata/KataApplication.java
@@ -0,0 +1,11 @@
+package com.bnp.kata;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataApplication.class, args);
+    }
+}
